55161f20dcf9c80db410bc6dc8aa2e48dd0a0265
Ianzoww <zeroxp534@gmail.com>
2024-09-08 11:55:52 -0300

    First Commit

---
+++ teste/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int numero = 5;
+long resultado = Fatorial(numero);
+Console.WriteLine($"O fatorial de {numero} é {resultado}");
+
+
+long Fatorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Fatorial(n - 1);
+    }
+}
